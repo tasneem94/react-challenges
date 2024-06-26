52c36bff9cdfb21b2c1d850097af504cde0d55fb
tasneem94 <tasneemsakif31@gmail.com>
2024-06-26 17:02:30 +0600

    second commit

--- accordion/src/components/Tiles.jsx
+++ accordion/src/components/Tiles.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // import data from "../data/tilesData";
 const Tiles = ({ index, title, body }) => {
   const [showBody, setShowBody] = useState(false);
   const onClickHandler = () => {
-    setShowBody(!showBody);
+    setShowBody((prevShowBody) => !prevShowBody);
   };
   return (
     <>
       <div className="tiles">
         <div className="title">{title}</div>
         <div className="show-btn" onClick={onClickHandler}>
-          +
+          {showBody ? (
+            <i class="fas fa-xmark"></i>
+          ) : (
+            <i class="fas fa-angle-down"></i>
+          )}
         </div>
       </div>
       {showBody && <div className="tiles body-text">{body}</div>}
