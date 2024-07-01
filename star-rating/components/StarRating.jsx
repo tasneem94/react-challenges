1d4bc47d7b732c328c52a67b9be3c97e0189d88e
tasneem94 <tasneemsakif31@gmail.com>
2024-07-01 21:40:17 +0600

    star rating added

--- star-rating/components/StarRating.jsx
+++ star-rating/components/StarRating.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
+
 const StarRating = ({ count }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [isHover, isSetHover] = useState(false);
+
+  const handleClick = (index) => {
+    setRating(index);
+  };
+  const handleMouseMove = (index) => {
+    setHover(index);
+    isSetHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(rating);
+    isSetHover(false);
+  };
+
   const stars = [...Array(count)].map((_, index) => {
-    return <span key={index} className="fa fa-star"></span>;
+    index += 1;
+    return (
+      <span
+        key={index}
+        className={
+          "fa fa-star" +
+          ((!isSetHover && rating >= index) || hover >= index ? " active" : " ")
+        }
+        onClick={() => handleClick(index)}
+        onMouseMove={() => handleMouseMove(index)}
+        onMouseLeave={handleMouseLeave}
+      ></span>
+    );
   });
   return <div className="star-container">{stars}</div>;
 };
