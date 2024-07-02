b81df7b36b44744b5afeed23daf5bfad2c30a93f
tasneem94 <tasneemsakif31@gmail.com>
2024-07-02 20:11:44 +0600

    first commit

---
+++ image-slider/src/App.jsx
@@ -0,0 +1,11 @@
+import ImageSlider from "./components/ImageSlider";
+
+function App() {
+  return (
+    <div className="container">
+      <ImageSlider />
+    </div>
+  );
+}
+
+export default App;
---
+++ image-slider/src/components/ImageSlider.jsx
@@ -0,0 +1,5 @@
+const ImageSlider = () => {
+  return <div className="image-container">Img</div>;
+};
+
+export default ImageSlider;
