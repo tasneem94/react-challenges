50985cc4e1a1cf2f59d4a89d53df75d5dc18ffe6
tasneem94 <tasneemsakif31@gmail.com>
2024-08-26 17:57:21 +0600

    syntax error solved

--- food-recipe-app/src/main.jsx
+++ food-recipe-app/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { GlobalState } from "./context/globalContext.jsx";
+import { GlobalState } from "./context/GlobalContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
